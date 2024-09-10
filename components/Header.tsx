dcd3fd3fc94942285a9f990da4e44f2644e1e4ec
Stefan Larsen <stefan.larsen@northstake.dk>
2024-09-10 15:59:04 +0200

    add walletConnect

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,11 @@ import { useApi } from '../context/ApiContext'
 import { AccountEntity } from '@northstake/northstakeapi'
 import { FaUserCircle, FaEthereum, FaCoins } from 'react-icons/fa'
 import LogoutButton from './Logoutbutton'
-import { Eip1193Provider, ethers } from 'ethers'
+import { ethers } from 'ethers'
 import { toast } from 'react-toastify'
-
-// Declare global ethereum property
-declare global {
-  interface Window {
-    ethereum?: Eip1193Provider
-  }
-}
+import { useConnect, useAccount, useDisconnect, useWriteContract, useWaitForTransactionReceipt } from 'wagmi'
+import { readContract } from '@wagmi/core'
+import { config } from '@/config/wagmi'
 
 // ABI of the smart contract
 const contractABI = [
@@ -49,16 +45,79 @@ const contractABI = [
 
 const Header = () => {
   const { api } = useApi()
+  const { connect, connectors } = useConnect()
+  const { address, isConnected } = useAccount()
+  const { disconnect } = useDisconnect()
+  const { data: hash, isPending, writeContract, isError } = useWriteContract()
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash })
 
   const [userInfo, setUserInfo] = useState<AccountEntity | null>(null)
   const [server, setServer] = useState('test')
   const [dropdownOpen, setDropdownOpen] = useState(false)
   const dropdownRef = useRef<HTMLDivElement>(null)
-  const [contract, setContract] = useState<ethers.Contract | null>(null)
   const [walletModalOpen, setWalletModalOpen] = useState(false)
   const [selectedAction, setSelectedAction] = useState<string | null>(null)
-  const [isMetaMaskConnected, setIsMetaMaskConnected] = useState(false)
   const [availableRewards, setAvailableRewards] = useState<string>('0')
+  const [contractAddress, setContractAddress] = useState<`0x${string}` | null>(null)
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  const depositToContract = () => {
+    alert('Depositing to contract')
+
+    writeContract({
+      address: contractAddress as `0x${string}`,
+      abi: contractABI,
+      functionName: 'deposit',
+      args: [ethers.parseEther('32')]
+    })
+  }
+
+  const acceptExitFromContract = (proposalId: string) => {
+    writeContract({
+      address: contractAddress as `0x${string}`,
+      abi: contractABI,
+      functionName: 'acceptExit',
+      args: [proposalId]
+    })
+  }
+
+  const collectRewardsFromContract = () => {
+    return writeContract({
+      address: contractAddress as `0x${string}`,
+      abi: contractABI,
+      functionName: 'collectRewards'
+    })
+  }
+
+  useEffect(() => {
+    if (isPending) {
+      toast.info('Transaction sent to wallet...')
+    }
+  }, [isPending])
+
+  useEffect(() => {
+    if (isConfirming) {
+      toast.info('Waiting for transaction confirmation on chain...')
+    }
+  }, [isConfirming])
+
+  useEffect(() => {
+    if (isConfirmed) {
+      toast.success('Transaction confirmed!')
+    }
+  }, [isConfirmed])
+
+  useEffect(() => {
+    if (isError) {
+      //clear all other toast notifications
+      toast.dismiss()
+      toast.error('Transaction rejected in wallet!')
+    }
+  }, [isError])
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -86,6 +145,13 @@ const Header = () => {
   }, [api])
 
   useEffect(() => {
+    if (isConnected && userInfo?.smartContracts?.[0]?.address) {
+      console.log('SETTING CONTRACT ADDRESS', userInfo.smartContracts[0].address)
+      setContractAddress(userInfo.smartContracts[0].address as `0x${string}`)
+    }
+  }, [isConnected, userInfo])
+
+  useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
         setDropdownOpen(false)
@@ -98,54 +164,50 @@ const Header = () => {
     }
   }, [])
 
-  const modalRef = useRef<HTMLDivElement>(null);
+  /* const modalRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-        closeWalletModal();
+        closeWalletModal()
       }
-    };
+    }
 
     if (walletModalOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside)
     }
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [walletModalOpen]);
-
-  const connectToMetaMask = async () => {
-    if (typeof window.ethereum !== 'undefined') {
-      try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' })
-        const provider = new ethers.BrowserProvider(window.ethereum)
-        const signer = await provider.getSigner()
-        setIsMetaMaskConnected(true)
-
-        const contractAddress = userInfo?.smartContracts?.[0]?.address
-        if (contractAddress) {
-          const contract = new ethers.Contract(contractAddress, contractABI, signer)
-          setContract(contract)
-          await updateRewards(contract)
-        }
-      } catch (error) {
-        console.error('Failed to connect to MetaMask', error)
-        toast.error('Failed to connect to MetaMask. Please try again.')
-      }
-    } else {
-      toast.error('Please install MetaMask!')
+      document.removeEventListener('mousedown', handleClickOutside)
     }
-  }
+  }, [walletModalOpen]) */
 
-  const handleWalletButtonClick = () => {
-    if (!isMetaMaskConnected) {
-      connectToMetaMask()
-    } else {
+  useEffect(() => {
+    if (isConnected && contractAddress) {
+      console.log('connected and contract address set to ', contractAddress)
+      updateRewards()
+    }
+  }, [isConnected, contractAddress])
+
+  /*   const handleConnectWallet = async () => {
+    console.log('isConnected', isConnected)
+    console.log(connectors)
+    if (isConnected) {
       setWalletModalOpen(true)
+    } else {
+      const walletConnectConnector = connectors.find(connector => connector.type === 'walletConnect')
+
+      if (walletConnectConnector) {
+        console.log('walletConnectConnector', walletConnectConnector)
+        try {
+          await connect({ connector: walletConnectConnector })
+        } catch (error) {
+          console.error('Failed to connect wallet', error)
+          toast.error('Failed to connect wallet. Please try again.')
+        }
+      }
     }
-  }
+  } */
 
   const handleServerChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setServer(e.target.value)
@@ -164,58 +226,50 @@ const Header = () => {
   }
 
   const handleWalletAction = async () => {
-    if (!contract) return
-
     try {
       let tx
       switch (selectedAction) {
         case 'deposit':
-          tx = await contract.deposit({ value: ethers.parseEther('32') })
+          tx = await depositToContract()
           break
         case 'acceptExit':
           const proposalId = prompt('Enter the proposal ID:')
           if (!proposalId) return
-          tx = await contract.acceptExit(proposalId)
+          tx = await acceptExitFromContract(proposalId)
           break
         case 'collectRewards':
-          tx = await contract.collectRewards()
+          tx = await collectRewardsFromContract()
           break
       }
-      await tx.wait()
+      console.log(tx)
       toast.success(`${selectedAction} successful!`)
       closeWalletModal()
     } catch (error) {
       console.error(`${selectedAction} failed`, error)
-
-      // Check if the error message contains the specific string
       if (error instanceof Error && error.message.includes('action="estimateGas"')) {
         toast.error('Please ensure your wallet is whitelisted before attempting this action.')
         return
       }
-
       toast.error(`${selectedAction} failed. Please try again.`)
     }
   }
 
-  const updateRewards = async (contract: ethers.Contract) => {
-    try {
-      const rewards = await contract.rewards()
-      setAvailableRewards(ethers.formatEther(rewards))
-    } catch (error) {
-      console.error('Failed to fetch rewards', error)
-    }
+  const updateRewards = async () => {
+    const output = await readContract(config, {
+      address: contractAddress as `0x${string}`,
+      abi: contractABI,
+      functionName: 'rewards'
+    })
+    console.log('rewards', output)
+    setAvailableRewards(ethers.formatEther(output as bigint))
   }
-  const handleCollectRewards = async () => {
-    if (contract) {
-      try {
-        const tx = await contract.collectRewards()
-        await tx.wait()
-        toast.success('Rewards collected successfully!')
-        await updateRewards(contract)
-      } catch (error) {
-        console.error('Collect rewards failed', error)
-        toast.error('Collect rewards failed. Please try again.')
-      }
+  const handleCollectRewards = () => {
+    try {
+      const result = collectRewardsFromContract()
+      console.log('result', result)
+    } catch (error) {
+      console.error('Collect rewards failed', error)
+      toast.error('Collect rewards failed. Please try again.')
     }
   }
 
@@ -229,10 +283,13 @@ const Header = () => {
           <option value='production'>Production</option>
         </select>
       </div>
+      <div className='flex justify-center'>{isMounted && address ? address.toString() : ''}</div>
       <div className='flex items-center'>
         <button
-          className={`ml-4 p-2 rounded flex items-center ${isMetaMaskConnected ? 'text-white' : 'text-gray-500'}`}
-          onClick={handleWalletButtonClick}
+          className={`ml-4 p-2 rounded flex items-center ${isConnected ? 'text-white' : 'text-gray-500'}`}
+          onClick={() => {
+            setWalletModalOpen(true)
+          }}
         >
           <FaEthereum className='text-2xl' />
         </button>
@@ -255,6 +312,7 @@ const Header = () => {
                     Smart Contract
                   </a>
                 </p>
+
                 <div className='flex justify-center'>
                   <LogoutButton />
                 </div>
@@ -266,8 +324,8 @@ const Header = () => {
 
       {walletModalOpen && (
         <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
-  <div ref={modalRef} className='bg-white text-black rounded-lg shadow-lg p-6 w-96'>
-            {isMetaMaskConnected && (
+          <div className='bg-white text-black rounded-lg shadow-lg p-6 w-96'>
+            {isConnected && (
               <div className='flex items-center justify-between mb-4' title='Available Rewards'>
                 <div className='flex items-center'>
                   <FaCoins className='text-yellow-400 mr-2' />
@@ -284,22 +342,47 @@ const Header = () => {
               </div>
             )}
             <h2 className='text-2xl font-bold mb-6'>Wallet Actions</h2>
-            <div className='space-y-4'>
-              <button
-                onClick={() => openWalletModal('deposit')}
-                className='w-full p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 flex items-center justify-center'
-              >
-                <FaEthereum className='mr-2' />
-                <span>Deposit 32 ETH (new validator)</span>
-              </button>
-              <button
-                onClick={() => openWalletModal('acceptExit')}
-                className='w-full p-3 bg-green-500 text-white rounded-lg hover:bg-green-600 flex items-center justify-center'
-              >
-                <FaEthereum className='mr-2' />
-                <span>Accept exit proposal</span>
-              </button>
-            </div>
+            {!isConnected && (
+              <div className='mb-4'>
+                <h3 className='text-lg font-semibold mb-2'>Connect Wallet</h3>
+
+                <div className='space-y-2'>
+                  {connectors.map(connector => (
+                    <button
+                      key={connector.id}
+                      onClick={() => connect({ connector })}
+                      className='w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+                    >
+                      {connector.name}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            )}
+            {isConnected && (
+              <div className='space-y-4'>
+                <button
+                  onClick={() => openWalletModal('deposit')}
+                  className='w-full p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 flex items-center justify-center'
+                >
+                  <FaEthereum className='mr-2' />
+                  <span>Deposit 32 ETH (new validator)</span>
+                </button>
+                <button
+                  onClick={() => openWalletModal('acceptExit')}
+                  className='w-full p-3 bg-green-500 text-white rounded-lg hover:bg-green-600 flex items-center justify-center'
+                >
+                  <FaEthereum className='mr-2' />
+                  <span>Accept exit proposal</span>
+                </button>
+                <button
+                  onClick={() => disconnect()}
+                  className='w-full p-3 bg-red-500 text-white rounded-lg hover:bg-red-600 flex items-center justify-center'
+                >
+                  Disconnect Wallet
+                </button>
+              </div>
+            )}
             <button
               onClick={closeWalletModal}
               className='mt-6 w-full p-3 bg-gray-300 text-gray-800 rounded-lg hover:bg-gray-400'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,22 +4,29 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import '../app/globals.css'
 import Sidebar from '@/components/Sidebar'
-
 import Header from '@/components/Header'
+import { config } from '@/config/wagmi'
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApiProvider>
-        
-      <div className='flex h-screen bg-gray-100'>
-        <Sidebar />
-        <div className='flex-1 flex flex-col overflow-hidden'>
-          <Header />
+      <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <div className='flex h-screen bg-gray-100'>
+          <Sidebar />
+          <div className='flex-1 flex flex-col overflow-hidden'>
+            <Header />
 
-          <Component {...pageProps} />
-          <ToastContainer />
+            <Component {...pageProps} />
+            <ToastContainer  style={{zIndex: 10000}} limit={1} />
+          </div>
         </div>
-      </div>
+        </QueryClientProvider>
+      </WagmiProvider>
     </ApiProvider>
   )
 }
