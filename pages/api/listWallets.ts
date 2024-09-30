f49ef659b167fb2646a226d72d5121de334e925f
Stefan Larsen <stefan.larsen@northstake.dk>
2024-09-30 09:33:11 +0200

    listWallets

--- pages/api/listWallets.ts
+++ pages/api/listWallets.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { initializeApi, listWallets as list } from '../../app/api';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+  if (req.method === 'GET') {
 
     try {
       const api =  initializeApi()
